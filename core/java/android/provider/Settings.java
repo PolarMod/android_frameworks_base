91c2db6c2540a6fb9b6612e4b01bf5ecbfcd147b
Andrewerr <p01ar@p01ar.net>
2022-01-09 18:02:49 +0200

    Revert "Network Traffic: Add Option for Users to select size of Single Traffic (Up/Down) Font Size"
    This reverts commit bc6f49ef6e3653c25bc8530c1531d9d9366d3c05.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5202,7 +5202,6 @@ public final class Settings {
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_STATE);
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_TYPE);
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD);
-            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_FONT_SIZE);
         }
 
         /**
