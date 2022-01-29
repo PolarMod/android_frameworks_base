a203d8f2c4dc549992b858e06cf1d5fb4794c380
Andrewerr <p01ar@p01ar.net>
2022-01-29 16:18:06 +0200

    [frameworks][settings]: removed duplicated constants

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5105,13 +5105,6 @@ public final class Settings {
         public static final String LOCKSCREEN_WEATHER_STYLE = "lockscreen_weather_style";
 
         /**
-         * 0: OmniJaws Style
-         * 1: KeyguardSlice Style
-         * @hide
-         */
-        public static final String LOCKSCREEN_WEATHER_STYLE = "lockscreen_weather_style";
-
-        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
