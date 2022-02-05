49bc15663fe777098b34f56b32e8ff9d052d2ce1
Andrewerr <p01ar@p01ar.net>
2022-02-05 20:35:40 +0200

    Revert "Allow overriding default volume dialog position"
    This reverts commit 5a5e49f5b68d0f03a575008add773f1fecdb227d.

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -409,7 +409,7 @@ public class VolumeDialogImpl implements VolumeDialog,
     public void onTuningChanged(String key, String newValue) {
         switch (key) {
             case VOLUME_PANEL_ON_LEFT:
-                final boolean volumePanelOnLeft = TunerService.parseIntegerSwitch(newValue, isAudioPanelOnLeftSide());
+                final boolean volumePanelOnLeft = TunerService.parseIntegerSwitch(newValue, false);
                 if (mVolumePanelOnLeft != volumePanelOnLeft) {
                     mVolumePanelOnLeft = volumePanelOnLeft;
                     mHandler.post(() -> {
@@ -420,10 +420,6 @@ public class VolumeDialogImpl implements VolumeDialog,
             default:
                 break;
         }
-    }
-
-    private boolean isAudioPanelOnLeftSide() {
-        return mContext.getResources().getBoolean(R.bool.config_audioPanelOnLeftSide);
     }
 
     protected ViewGroup getDialogView() {
