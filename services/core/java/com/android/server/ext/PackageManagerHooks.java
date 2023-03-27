5df1bad2c4edc7dde0e3f42d0f8a8911f2a6a199
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-03-27 16:29:32 +0300

    integrate Google's eUICC LPA app
    Depends on GmsCompat.

---
+++ services/core/java/com/android/server/ext/PackageManagerHooks.java
@@ -0,0 +1,91 @@
+package com.android.server.ext;
+
+import android.Manifest;
+import android.annotation.Nullable;
+import android.annotation.UserIdInt;
+import android.content.pm.PackageManager;
+import android.os.UserHandle;
+import android.util.ArraySet;
+
+import com.android.internal.util.GoogleEuicc;
+import com.android.server.pm.pkg.PackageStateInternal;
+import com.android.server.pm.pkg.parsing.ParsingPackage;
+
+public class PackageManagerHooks {
+
+    // Called when package enabled setting is deserialized from storage
+    @Nullable
+    public static Integer maybeOverridePackageEnabledSetting(String pkgName, @UserIdInt int userId) {
+        switch (pkgName) {
+            case GoogleEuicc.EUICC_SUPPORT_PIXEL_PKG_NAME:
+                if (userId == UserHandle.USER_SYSTEM) {
+                    // EuiccSupportPixel handles firmware updates and should always be enabled.
+                    // It was previously unconditionally disabled after reboot.
+                    return PackageManager.COMPONENT_ENABLED_STATE_DEFAULT;
+                } else {
+                    // one of the previous OS versions enabled EuiccSupportPixel in all users
+                    return PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+                }
+            case GoogleEuicc.LPA_PKG_NAME:
+                // Google's LPA should be always disabled after reboot
+                return PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+            default:
+                return null;
+        }
+    }
+
+    // Called when package parsing is completed
+    public static void amendParsedPackage(ParsingPackage pkg) {
+        String pkgName = pkg.getPackageName();
+
+        switch (pkgName) {
+            case GoogleEuicc.EUICC_SUPPORT_PIXEL_PKG_NAME:
+                // EuiccSupportPixel uses INTERNET perm only as part of its dev mode
+                removeUsesPermissions(pkg, Manifest.permission.INTERNET);
+                return;
+            case GoogleEuicc.LPA_PKG_NAME:
+                // this is the same as android:enabled="false" in <application> AndroidManifest tag,
+                // it makes the package disabled by default on first boot, when there's no
+                // serialized package state
+                pkg.setEnabled(false);
+                return;
+            default:
+                return;
+        }
+    }
+
+    public static void removeUsesPermissions(ParsingPackage pkg, String... perms) {
+        var set = new ArraySet<>(perms);
+        pkg.getRequestedPermissions().removeAll(set);
+        pkg.getUsesPermissions().removeIf(p -> set.contains(p.getName()));
+    }
+
+    // Called when AppsFilter decides whether to restrict package visibility
+    public static boolean shouldFilterAccess(@Nullable PackageStateInternal callingPkgSetting,
+                                             ArraySet<PackageStateInternal> callingSharedPkgSettings,
+                                             PackageStateInternal targetPkgSetting) {
+        if (callingPkgSetting != null && restrictedVisibilityPackages.contains(callingPkgSetting.getPackageName())) {
+            if (!targetPkgSetting.isSystem()) {
+                return true;
+            }
+        }
+
+        if (restrictedVisibilityPackages.contains(targetPkgSetting.getPackageName())) {
+            if (callingPkgSetting != null) {
+                return !callingPkgSetting.isSystem();
+            } else {
+                for (int i = callingSharedPkgSettings.size() - 1; i >= 0; i--) {
+                    if (!callingSharedPkgSettings.valueAt(i).isSystem()) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    // Packages in this array are restricted from interacting with and being interacted by non-system apps
+    private static final ArraySet<String> restrictedVisibilityPackages = new ArraySet<>(new String[] {
+        GoogleEuicc.EUICC_SUPPORT_PIXEL_PKG_NAME,
+    });
+}
