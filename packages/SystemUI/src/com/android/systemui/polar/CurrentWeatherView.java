e070332cbecf9c21065117743273ef4b241779b4
Andrewerr <toddot@protonmail.com>
2023-06-22 20:58:25 +0200

    [FIX] [weather]: fix package name for CurrentWeatherView

--- packages/SystemUI/src/com/android/systemui/polar/CurrentWeatherView.java
+++ packages/SystemUI/src/com/android/systemui/polar/CurrentWeatherView.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.systemui.crdroid;
+package com.android.systemui.polar;
 
 import android.content.Context;
 import android.database.ContentObserver;
