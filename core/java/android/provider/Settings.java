b9797159ce34a86836e3053f5d23604805bdbb60
Andrewerr <toddot@protonmail.com>
2023-08-16 14:29:47 +0200

    [FIX] [settings]: fixed duplicates in settings provider

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5708,25 +5708,10 @@ public final class Settings {
         public static final String BLACK_STATUSBAR = "black_statusbar";
 
         /**
-         * Bring back old style mobile data indicators.
+         * Old style mobile data indicators.
          * @hide
          */
         public static final String USE_OLD_MOBILETYPE = "use_old_mobiletype";
-
-        /**
-         * @hide
-         */
-        public static final String UDFPS_ANIM = "udfps_anim";
-
-        /**
-         * @hide
-         */
-        public static final String UDFPS_ANIM_STYLE = "udfps_anim_style";
-
-        /**
-         * @hide
-         */
-        public static final String UDFPS_ICON = "udfps_icon";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
